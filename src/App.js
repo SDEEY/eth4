736db1927375a10f72b9d5bd07c8e70be30674ce
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-03-19 01:52:32 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = 0.0369
-const image = 'https://pbs.twimg.com/profile_images/1632779411047972878/CnsV8cpc_400x400.png'
-const Title = 'BIRDYBITES'
-const supply = 1772
+const ethAmount = 0.02
+const image = 'https://pbs.twimg.com/profile_images/1538764942412656641/LzWIx-A7_400x400.jpg'
+const Title = 'WONDERLAND METAVERSE'
+const supply = 999
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
