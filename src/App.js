87fd74609093caf07c2ad704c3c391edb09b71b6
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-03-18 19:50:39 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = 0.05
-const image = 'https://pbs.twimg.com/profile_images/1605721243826589696/GvxYCGAI_400x400.png'
-const Title = 'DREAMS NEVER DIEDREAMS NEVER DIE'
-const supply = 91
+const ethAmount = 0.0117
+const image = 'https://pbs.twimg.com/profile_images/1636724695516037120/KxpgwmQJ_400x400.jpg'
+const Title = 'HANA NO HATA'
+const supply = 171
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
