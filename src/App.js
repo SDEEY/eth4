9b970920f143a15c702726f9479b2106ca873152
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-03-18 23:55:58 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = 0.0117
-const image = 'https://pbs.twimg.com/profile_images/1636724695516037120/KxpgwmQJ_400x400.jpg'
-const Title = 'HANA NO HATA'
-const supply = 171
+const ethAmount = 0
+const image = 'https://asteroidheadz.com/mint/ola.gif'
+const Title = 'Asteroid Headz'
+const supply = 113
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
