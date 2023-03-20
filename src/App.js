4b5e3fc2a42b2b0457dbe235748a77603e7b31a1
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-03-21 01:59:53 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
 const ethAmount = '0'
-const image = 'https://pbs.twimg.com/profile_images/1562687015203885056/mTiwCDoo_400x400.jpg'
-const Title = 'STAY CATS'
+const image = 'https://pbs.twimg.com/profile_images/1615255731854544897/rCJvs1um_400x400.jpg'
+const Title = 'Merlin Protocol'
 const supply = 333
 
 document.title = Title
