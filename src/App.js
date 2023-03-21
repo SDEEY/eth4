e5365b07a91e7d42ab0ec2fcf999a97ab5d7e9d7
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-03-21 21:18:47 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0.012'
+const ethAmount = '0.009 / 0.011'
 const image = 'https://pbs.twimg.com/profile_images/1609792331544150016/tbHmM4BO_400x400.png'
 const Title = 'VERSION THR33'
 const supply = 2222
