83be08ed54a7e9d3e798b2644a7495c50c1b7296
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-03-28 21:38:18 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0'
-const image = 'https://pbs.twimg.com/profile_images/1639376138429693952/5B4ArDHk_400x400.jpg'
-const Title = 'Evil Kongs'
-const supply = 222
+const ethAmount = '0.03'
+const image = 'https://pbs.twimg.com/profile_images/1621926831783026688/gIgOQdtO_400x400.jpg'
+const Title = 'SoRekt.wtf'
+const supply = 500
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
