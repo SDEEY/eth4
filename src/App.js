36df4a254fde2378ce42cd5ac66744eebd8a2ecb
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-03-28 23:53:01 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0.03'
-const image = 'https://pbs.twimg.com/profile_images/1621926831783026688/gIgOQdtO_400x400.jpg'
-const Title = 'SoRekt.wtf'
-const supply = 500
+const ethAmount = '0.04'
+const image = 'https://pbs.twimg.com/profile_images/1543624213394853889/qt4_jxsh_400x400.jpg'
+const Title = 'Neo Films NFT'
+const supply = 2700
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
