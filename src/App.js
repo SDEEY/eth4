87bcecb99227dc37b5b0349bb5dffa3a689b9763
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-03-30 00:47:03 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0'
-const image = 'https://pbs.twimg.com/profile_images/1580493129978880000/TLncC3mM_400x400.jpg'
-const Title = 'Moonlit Games'
-const supply = 471
+const ethAmount = '0.325 / 0.35'
+const image = 'https://pbs.twimg.com/profile_images/1630377578597949440/1ncXPKhI_400x400.jpg'
+const Title = 'Chronos'
+const supply = 671
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
