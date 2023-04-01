796dfd55d770e24dfacca5f049e6c0747b28be0e
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-04-01 22:51:32 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0.1'
-const image = 'https://media.discordapp.net/attachments/1053955903151415316/1085566810608377986/ezgif-3-93bff57039.gif?width=590&height=590'
-const Title = 'Be Zelensky'
-const supply = 10000
+const ethAmount = '0'
+const image = 'https://media.discordapp.net/attachments/887683434556841994/1088506468648632320/1897.gif'
+const Title = 'META SPACE COMMANDER GENESIS'
+const supply = 4444
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
