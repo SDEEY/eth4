e14e418cfca1133d9acffd2a0f1b749ef4ae8d2d
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-04-08 02:04:40 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
 const ethAmount = '0.04'
-const image = 'https://pbs.twimg.com/profile_images/1606373681592139777/IQQorKdt_400x400.jpg'
-const Title = 'BUDDY BUCKS'
-const supply = 1155
+const image = 'https://pbs.twimg.com/profile_images/1559517255775358978/bT6gqU8a_400x400.png'
+const Title = 'DEEPSPACE'
+const supply = 711
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
