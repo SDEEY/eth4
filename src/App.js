07a1a7e3059a5b50ce74c5e37ed58ca82ba4d1e1
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-04-10 21:47:47 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
 const ethAmount = '0'
-const image = 'https://pbs.twimg.com/profile_images/1622655913936175104/GnFNCYym_400x400.png'
+const image = 'https://mint.oddverse.xyz/static/media/mint.48ed0000974f31a26171.gif'
 const Title = 'oDD BOi'
 const supply = 999
 
