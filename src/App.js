4fda87d5bfeb333967c3e8bec67dd8824f0d95f6
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-04-11 23:41:32 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0.016'
-const image = 'https://pbs.twimg.com/profile_images/1637702463347212289/cfutA_5n_400x400.jpg'
-const Title = 'GLAMOROUS APES'
-const supply = 971
+const ethAmount = '0.039'
+const image = 'https://pbs.twimg.com/profile_images/1585261818594877444/gU3V7j74_400x400.jpg'
+const Title = 'K9ICKS'
+const supply = 111
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
