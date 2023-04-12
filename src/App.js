56d7978ee13aa05e57bb6a05c81c6196210cd931
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-04-12 21:45:02 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0.034'
-const image = 'https://pbs.twimg.com/profile_images/1614907426251341825/mslZua6f_400x400.jpg'
-const Title = 'Ghost Child'
-const supply = 590
+const ethAmount = '0.011'
+const image = 'https://pbs.twimg.com/profile_images/1615431212637511685/Vn8Rtx_M_400x400.jpg'
+const Title = 'ZORA'
+const supply = 100
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
