424c2a8494e30f20e54a307b9636e08005291309
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-04-20 20:24:27 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0.089'
-const image = 'https://pbs.twimg.com/profile_images/1572185257176190979/z9zpJ45K_400x400.jpg'
-const Title = 'MetaZoku'
-const supply = 2855
+const ethAmount = '0.025'
+const image = 'https://pbs.twimg.com/profile_images/1610442440191930370/XziTGu2t_400x400.jpg'
+const Title = 'DRuNkiEs'
+const supply = 1844
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
