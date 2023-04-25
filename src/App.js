fb847c2a466dc05fe169e15bc2f6717cbd38fa75
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-04-25 16:50:53 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
 const ethAmount = '0'
-const image = 'https://pbs.twimg.com/profile_images/1526687323240398849/qWck0nK5_400x400.jpg'
+const image = 'https://d1fdloi71mui9q.cloudfront.net/wULFDGPdRI4EWrAzDUzk_2R829GtNxywy9rBq'
 const Title = 'DEFIT'
 const supply = 1000
 
