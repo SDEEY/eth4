65387a32a0d901284070f3fc0c5af91e033204fd
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-04-26 20:47:43 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0.0325'
+const ethAmount = '0.022'
 const image = 'https://pbs.twimg.com/profile_images/1626150122500534275/KK9MD66l_400x400.jpg'
 const Title = 'MySquare'
 const supply = 8888
