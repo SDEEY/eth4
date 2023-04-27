a0a4bc8a9d373bf6c74c1250b04baf7a08976c10
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-04-27 18:49:58 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0.06'
-const image = 'https://pbs.twimg.com/profile_images/1630095448945102849/jKVVpD2H_400x400.jpg'
-const Title = 'Hype Boy NFT'
-const supply = 1000
+const ethAmount = '0'
+const image = 'https://pbs.twimg.com/profile_images/1633917126535790598/EGPSdcbv_400x400.jpg'
+const Title = 'Faraway'
+const supply = 888
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
