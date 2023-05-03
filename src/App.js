9bffe0d92568ea9158dfdadc1908a42ae93a1b36
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-05-04 00:35:58 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0.03'
-const image = 'https://pbs.twimg.com/profile_images/1642700244533272577/9tH5LGFs_400x400.jpg'
-const Title = 'Freckle TV '
-const supply = 4180
+const ethAmount = '0.15'
+const image = 'https://pbs.twimg.com/profile_images/1625875594129858562/_Xzqqhz1_400x400.jpg'
+const Title = 'Luna Hunters NFT'
+const supply = 888
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
