d85c0cec1c986e81ee985d5c6b85b723e458e626
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-05-06 00:55:49 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0.033'
-const image = 'https://pbs.twimg.com/profile_images/1617641690969276417/J7Ejca9e_400x400.jpg'
-const Title = 'Apollumia'
-const supply = 1000
+const ethAmount = '0.025'
+const image = 'https://pbs.twimg.com/profile_images/1638709959150977033/CFMSS642_400x400.jpg'
+const Title = 'Dope Dapper Ducks'
+const supply = 3180
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
