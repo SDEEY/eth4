f58a17f39e9441c16aabe7576b98d46d904bf1cb
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-05-12 22:51:40 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0'
-const image = 'https://pbs.twimg.com/profile_images/1656667875967631360/bdfhQqv0_400x400.jpg'
-const Title = 'PépéNFT'
-const supply = 444
+const ethAmount = '0.0666'
+const image = 'https://pbs.twimg.com/profile_images/1650728092372185090/s-ofXAxa_400x400.jpg'
+const Title = 'Meka Anura'
+const supply = 666
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
