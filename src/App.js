1fb0404505b73a978cb7425eaaaf594c78feda7a
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-05-13 01:07:24 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0.0666'
-const image = 'https://pbs.twimg.com/profile_images/1650728092372185090/s-ofXAxa_400x400.jpg'
-const Title = 'Meka Anura'
-const supply = 666
+const ethAmount = '0.08'
+const image = 'https://pbs.twimg.com/profile_images/1581704156267839488/rMYDPqVC_400x400.jpg'
+const Title = 'Cyber Syndicate VX'
+const supply = 2222
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
