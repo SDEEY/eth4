9d7d616ed9052770491ebefb47fd2331bfe1a75a
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-05-26 20:30:29 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0.077'
-const image = 'https://pbs.twimg.com/profile_images/1638585680862355469/rk5WfhCW_400x400.jpg'
-const Title = 'FIGUREGOT'
-const supply = 2171
+const ethAmount = '0'
+const image = 'https://pbs.twimg.com/profile_images/1631004854268166170/NzVFcpkZ_400x400.jpg'
+const Title = 'Players of the Game'
+const supply = 222
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
