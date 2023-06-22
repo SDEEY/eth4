520dbccc3964fdbf828c88c85d92d45c89dc41a0
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-06-22 14:59:26 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0.001'
-const image = 'https://pbs.twimg.com/profile_images/1661421486526312448/HeuiUwXv_400x400.jpg'
-const Title = 'The Guardians'
-const supply = 999
+const ethAmount = '0'
+const image = 'https://pbs.twimg.com/profile_images/1628693672782102530/YeuYXB28_400x400.jpg'
+const Title = 'Peaky Birds'
+const supply = 371
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
