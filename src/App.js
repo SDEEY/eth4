82d42b6a45940189466ecab9c8aa316fdcad2074
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-06-23 18:44:27 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0.0232'
-const image = 'https://pbs.twimg.com/profile_images/1633906535234453510/5P1d2JHO_400x400.png'
-const Title = 'paladinpunks.eth'
-const supply = 371
+const ethAmount = '0.055/0.06'
+const image = 'https://pbs.twimg.com/profile_images/1570570759767982080/vhrADMzA_400x400.jpg'
+const Title = 'Kyokai'
+const supply = 1000
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
