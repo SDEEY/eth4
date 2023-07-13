f56b0ee65d36d5cb4b35224aa39e189628a510cd
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-07-13 14:08:21 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0.07'
-const image = 'https://pbs.twimg.com/profile_images/1629519175184941061/spVp5IOH_400x400.jpg'
-const Title = 'the Order of Awakening'
-const supply = 471
+const ethAmount = '0.015'
+const image = 'https://pbs.twimg.com/profile_images/1644427103729774604/Jn0kh25p_400x400.png'
+const Title = 'OrangeHare'
+const supply = 171
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
