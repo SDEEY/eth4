5ba249a0dd4ae7b90ea37d2c46f7e80165f9c804
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-07-21 00:23:07 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0.01/0.015'
-const image = 'https://pbs.twimg.com/profile_images/1639056038639828992/dtf7QbhM_400x400.jpg'
-const Title = 'Galactic Council'
-const supply = 2181
+const ethAmount = '0.005'
+const image = 'https://pbs.twimg.com/profile_images/1602700328415399937/abw487Ue_400x400.jpg'
+const Title = 'Aroki Warriors'
+const supply = 555
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
