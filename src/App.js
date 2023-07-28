73b864b45a36f005e7c365538ec61aaeb5665681
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-07-28 20:58:52 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0.019'
-const image = 'https://pbs.twimg.com/profile_images/1615929867966504960/ekYmx2TC_400x400.jpg'
-const Title = 'Himalaya'
-const supply = 1971
+const ethAmount = '0'
+const image = 'https://pbs.twimg.com/profile_images/1658024491782664193/mqayIDTB_400x400.jpg'
+const Title = 'MetaCene'
+const supply = 171
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
